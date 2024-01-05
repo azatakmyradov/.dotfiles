a7f655527138868aa946c7f61e9a5b8d0e5ed3aa
Azat Akmyradov <azat@rebdev.com>
2024-01-05 15:34:40 -0500

    Change conflicting keymaps

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -70,7 +70,7 @@ vim.defer_fn(function()
       swap = {
         enable = true,
         swap_next = {
-          ['<leader>a'] = '@parameter.inner',
+          ['<leader>c'] = '@parameter.inner',
         },
         swap_previous = {
           ['<leader>A'] = '@parameter.inner',
