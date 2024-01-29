969a0c9716e6b4c52edea5ef07dd9538b5b393ab
Azat Akmyradov <azat@rebdev.com>
2024-01-28 23:59:10 -0500

    Extracted git signs config to seperate file

--- .config/nvim/lua/azatakmyradov/plugins.lua
+++ .config/nvim/lua/azatakmyradov/plugins.lua
@@ -1,275 +1,199 @@
 require('lazy').setup({
 
-  -- the colorscheme should be available when starting Neovim
-  {
-    'rose-pine/neovim',
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      require('azatakmyradov/colorscheme')
-    end,
-  },
-
-  -- Git related plugins
-  {
-    'tpope/vim-fugitive',
-    dependencies = {
-      'tpope/vim-rhubarb',
-    }
-  },
-
-  -- Install emmet-vim
-  { 'mattn/emmet-vim' },
-
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-
-  -- Add, change, and delete surrounding text.
-  { 'tpope/vim-surround' },
-
-  -- Automatically add closing brackets, quotes, etc.
-  {
-    'echasnovski/mini.nvim',
-    config = function()
-      require('mini.pairs').setup()
-    end
-  },
-
-  -- Useful commands like :Rename and :SudoWrite.
-  { 'tpope/vim-eunuch' },
-
-  -- Jump to the last location when opening a file.
-  { 'farmergreg/vim-lastplace' },
-
-  -- Automatically create parent dirs when saving.
-  { 'jessarcher/vim-heritage' },
-
-  -- Text objects for HTML attributes.
-  {
-    'whatyouhide/vim-textobj-xmlattr',
-    dependencies = {
-      'kana/vim-textobj-user'
-    }
-  },
-
-  -- Automatically set the working directory to the project root.
-  {
-    'airblade/vim-rooter',
-    config = function()
-      vim.g.rooter_manual_only = 1
-      vim.cmd('Rooter')
-    end,
-  },
-
-  -- Split arrays and methods onto multiple lines, or join them back up
-  {
-    'AndrewRadev/splitjoin.vim',
-    config = function()
-      vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
-      vim.g.splitjoin_trailing_comma = 1
-      vim.g.splitjoin_php_method_chain_full = 1
-    end,
-  },
-
-  -- Automatically fix indentation when pasting code.
-  {
-    'sickill/vim-pasta',
-    config = function()
-      vim.g.pasta_disabled_filetypes = { 'fugitive' }
-    end,
-  },
-
-  -- Language Server Protocol.
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    dependencies = {
-      { 'neovim/nvim-lspconfig' },             -- Required
-      { 'williamboman/mason.nvim' },           -- Optional
-      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-    },
-  },
-
-  {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
-
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-      'hrsh7th/cmp-buffer',
-      'onsails/lspkind-nvim',
-    },
-  },
-
-  {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Navigation
-        map({ 'n', 'v' }, ']c', function()
-          if vim.wo.diff then
-            return ']c'
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, desc = 'Jump to next hunk' })
-
-        map({ 'n', 'v' }, '[c', function()
-          if vim.wo.diff then
-            return '[c'
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, desc = 'Jump to previous hunk' })
-
-        -- Actions
-        -- visual mode
-        map('v', '<leader>hs', function()
-          gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'stage git hunk' })
-        map('v', '<leader>hr', function()
-          gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'reset git hunk' })
-        -- normal mode
-        map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
-        map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
-        map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
-        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-        map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
-        map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
-        map('n', '<leader>hb', function()
-          gs.blame_line { full = false }
-        end, { desc = 'git blame line' })
-        map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
-        map('n', '<leader>hD', function()
-          gs.diffthis '~'
-        end, { desc = 'git diff against last commit' })
-
-        -- Toggles
-        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-        map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
-
-        -- Text object
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
-      end,
-    },
-  },
-
-  {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'rose-pine',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
-
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
-
-  -- Fuzzy Finder (files, lsp, etc)
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope-live-grep-args.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
+    -- the colorscheme should be available when starting Neovim
+    {
+        'rose-pine/neovim',
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+            require('azatakmyradov/colorscheme')
         end,
-      },
     },
-  },
 
-  {
-    -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'JoosepAlviste/nvim-ts-context-commentstring',
-      'nvim-treesitter/nvim-treesitter-textobjects',
+    -- Git related plugins
+    {
+        'tpope/vim-fugitive',
+        dependencies = {
+            'tpope/vim-rhubarb',
+        }
     },
-    build = ':TSUpdate',
-  },
 
-  { 'ThePrimeagen/harpoon' },
+    -- Install emmet-vim
+    { 'mattn/emmet-vim' },
 
-  -- GitHub Copilot
-  {
-    'github/copilot.vim',
+    -- Add, change, and delete surrounding text.
+    { 'tpope/vim-surround' },
 
-    config = function()
-      vim.g.copilot_no_tab_map = true
-      vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-      vim.api.nvim_set_keymap("i", "<C-;>", 'copilot#Previous()', { silent = true, expr = true })
-      vim.api.nvim_set_keymap("i", "<C-'>", 'copilot#Next()', { silent = true, expr = true })
-    end
-  },
+    -- Automatically add closing brackets, quotes, etc.
+    {
+        'echasnovski/mini.nvim',
+        config = function()
+            require('mini.pairs').setup()
+        end
+    },
 
-  -- UndoTree
-  { 'mbbill/undotree' },
+    -- Useful commands like :Rename and :SudoWrite.
+    { 'tpope/vim-eunuch' },
 
-  -- Auto tag rename
-  {
-    'windwp/nvim-ts-autotag',
-    config = function()
-      require('nvim-ts-autotag').setup()
-    end
-  },
+    -- Jump to the last location when opening a file.
+    { 'farmergreg/vim-lastplace' },
 
-  -- Icons for files
-  { "nvim-tree/nvim-web-devicons" },
+    -- Automatically create parent dirs when saving.
+    { 'jessarcher/vim-heritage' },
 
-  -- ToDo Comments
-  {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("todo-comments").setup {}
-    end
-  },
+    -- Text objects for HTML attributes.
+    {
+        'whatyouhide/vim-textobj-xmlattr',
+        dependencies = {
+            'kana/vim-textobj-user'
+        }
+    },
 
-  -- NeoDev
-  { "folke/neodev.nvim", opts = {} },
+    -- Automatically set the working directory to the project root.
+    {
+        'airblade/vim-rooter',
+        config = function()
+            vim.g.rooter_manual_only = 1
+            vim.cmd('Rooter')
+        end,
+    },
 
-  -- Trouble
-  { "folke/trouble.nvim" }
+    -- Split arrays and methods onto multiple lines, or join them back up
+    {
+        'AndrewRadev/splitjoin.vim',
+        config = function()
+            vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+            vim.g.splitjoin_trailing_comma = 1
+            vim.g.splitjoin_php_method_chain_full = 1
+        end,
+    },
+
+    -- Automatically fix indentation when pasting code.
+    {
+        'sickill/vim-pasta',
+        config = function()
+            vim.g.pasta_disabled_filetypes = { 'fugitive' }
+        end,
+    },
+
+    -- Language Server Protocol.
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        dependencies = {
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+        },
+    },
+
+    {
+        -- Autocompletion
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            -- Snippet Engine & its associated nvim-cmp source
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+
+            -- Adds LSP completion capabilities
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-path',
+
+            -- Adds a number of user-friendly snippets
+            'rafamadriz/friendly-snippets',
+
+            'hrsh7th/cmp-nvim-lsp-signature-help',
+            'hrsh7th/cmp-buffer',
+            'onsails/lspkind-nvim',
+        },
+    },
+
+    { 'lewis6991/gitsigns.nvim' },
+
+    {
+        -- Set lualine as statusline
+        'nvim-lualine/lualine.nvim',
+        -- See `:help lualine.txt`
+        opts = {
+            options = {
+                icons_enabled = false,
+                theme = 'rose-pine',
+                component_separators = '|',
+                section_separators = '',
+            },
+        },
+    },
+
+    -- "gc" to comment visual regions/lines
+    { 'numToStr/Comment.nvim', opts = {} },
+
+    -- Fuzzy Finder (files, lsp, etc)
+    {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope-live-grep-args.nvim',
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build = 'make',
+                cond = function()
+                    return vim.fn.executable 'make' == 1
+                end,
+            },
+        },
+    },
+
+    {
+        -- Highlight, edit, and navigate code
+        'nvim-treesitter/nvim-treesitter',
+        dependencies = {
+            'JoosepAlviste/nvim-ts-context-commentstring',
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+        build = ':TSUpdate',
+    },
+
+    { 'ThePrimeagen/harpoon' },
+
+    -- GitHub Copilot
+    {
+        'github/copilot.vim',
+
+        config = function()
+            vim.g.copilot_no_tab_map = true
+            vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+            vim.api.nvim_set_keymap("i", "<C-;>", 'copilot#Previous()', { silent = true, expr = true })
+            vim.api.nvim_set_keymap("i", "<C-'>", 'copilot#Next()', { silent = true, expr = true })
+        end
+    },
+
+    -- UndoTree
+    { 'mbbill/undotree' },
+
+    -- Auto tag rename
+    {
+        'windwp/nvim-ts-autotag',
+        config = function()
+            require('nvim-ts-autotag').setup()
+        end
+    },
+
+    -- Icons for files
+    { "nvim-tree/nvim-web-devicons" },
+
+    -- ToDo Comments
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("todo-comments").setup {}
+        end
+    },
+
+    -- NeoDev
+    { "folke/neodev.nvim", opts = {} },
+
+    -- Trouble
+    { "folke/trouble.nvim" }
 }, {})
