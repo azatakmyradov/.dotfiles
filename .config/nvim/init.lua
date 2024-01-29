67956c70d4e9840ff487a5f0483049a12a6d7894
Azat Akmyradov <azat@rebdev.com>
2024-01-29 03:01:58 -0500

    Updated nvim config structure to fit for lazy.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,21 +3,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- [[ Install `lazy.nvim` plugin manager ]]
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
-
--- [[ Configure plugins ]]
-require('azatakmyradov/plugins')
+require('azatakmyradov.lazy')
 
 -- [[ Setting options ]]
 require('azatakmyradov/options')
