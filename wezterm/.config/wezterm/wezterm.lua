d95268cd6e3011882bff251e818891b6de4d6fd0
Azat Akmyradov <azat@rebdev.com>
2024-11-12 09:28:00 -0500

    Replace alacritty with wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,41 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("OperatorMono Nerd Font")
+config.font_size = 17
+config.line_height = 1.2
+config.enable_tab_bar = false
+
+config.window_padding = {
+	left = 2,
+	right = 2,
+	top = "1cell",
+	bottom = 0,
+}
+config.window_decorations = "RESIZE"
+
+config.ssh_domains = {
+	{
+		name = "rebdev",
+		remote_address = "20.75.54.163",
+		username = "azat",
+	},
+}
+
+config.keys = {
+	{
+		key = "m",
+		mods = "CMD",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
