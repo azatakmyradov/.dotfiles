a2e222ad62aab723f5855a348883da993bf40fac
Azat Akmyradov <azakmyradov@rklesolutions.com>
2025-03-29 16:44:24 -0400

    Add some ai stuff

--- config/nvim/lua/azatakmyradov/plugins/colorscheme.lua
+++ config/nvim/lua/azatakmyradov/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return { -- You can easily change to a different colorscheme.
-  'folke/tokyonight.nvim',
-  lazy = false,
-  priority = 1000,
-  init = function()
-    vim.cmd.colorscheme 'tokyonight'
+	"rose-pine/neovim",
+	lazy = false,
+	priority = 1000,
+	init = function()
+		vim.cmd.colorscheme("rose-pine")
 
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-  end,
+		-- You can configure highlights by doing something like:
+		vim.cmd.hi("Comment gui=none")
+	end,
 }
--- config/nvim/lua/azatakmyradov/plugins/init.lua
+++ config/nvim/lua/azatakmyradov/plugins/init.lua
@@ -1,79 +1,91 @@
 return {
-  { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
+	{ "tpope/vim-sleuth" }, -- Detect tabstop and shiftwidth automatically
 
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+	-- "gc" to comment visual regions/lines
+	{ "numToStr/Comment.nvim", opts = {} },
 
-  -- harpoon to access files quickly
-  { 'ThePrimeagen/harpoon' },
+	-- harpoon to access files quickly
+	{ "ThePrimeagen/harpoon" },
 
-  -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
+	-- See `:help gitsigns` to understand what the configuration keys do
+	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
 
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+	-- Highlight todo, notes, etc in comments
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
 
-  -- Git related plugins
-  {
-    'tpope/vim-fugitive',
-    dependencies = {
-      'tpope/vim-rhubarb',
-    },
-  },
+	-- Git related plugins
+	{
+		"tpope/vim-fugitive",
+		dependencies = {
+			"tpope/vim-rhubarb",
+		},
+	},
 
-  -- Undo Tree
-  {
-    'mbbill/undotree',
-  },
+	-- Undo Tree
+	{
+		"mbbill/undotree",
+	},
 
-  {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {
-      view_options = {
-        show_hidden = true,
-      },
-    },
-    -- Optional dependencies
-    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-  },
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {
+			view_options = {
+				show_hidden = true,
+			},
+		},
+		-- Optional dependencies
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	},
 
-  -- auto close html tags
-  {
-    'windwp/nvim-ts-autotag',
-    config = function()
-      require('nvim-ts-autotag').setup()
-    end,
-  },
+	-- auto close html tags
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
 
-  -- Tailwind Tools
-  {
-    'luckasRanarison/tailwind-tools.nvim',
-    name = 'tailwind-tools',
-    build = ':UpdateRemotePlugins',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-telescope/telescope.nvim', -- optional
-      'neovim/nvim-lspconfig', -- optional
-    },
-    opts = {}, -- your configuration
-  },
+	-- Tailwind Tools
+	{
+		"luckasRanarison/tailwind-tools.nvim",
+		name = "tailwind-tools",
+		build = ":UpdateRemotePlugins",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-telescope/telescope.nvim", -- optional
+			"neovim/nvim-lspconfig", -- optional
+		},
+		opts = {}, -- your configuration
+	},
 
-  {
-    'stevearc/dressing.nvim',
-    opts = {},
-  },
+	{
+		"stevearc/dressing.nvim",
+		opts = {},
+	},
+
+	{
+		"nvzone/typr",
+		dependencies = "nvzone/volt",
+		opts = {},
+		cmd = { "Typr", "TyprStats" },
+	},
 }
