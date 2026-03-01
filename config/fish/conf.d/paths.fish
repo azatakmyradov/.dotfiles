92001b7e85b8f6acf1032a77007bd0c41b5e28cf
Azat Akmyradov <azat@akmyradov.me>
2026-02-28 21:49:56 -0500

    chore(dotfiles): add local bin path and trim opencode plugins

--- config/fish/conf.d/paths.fish
+++ config/fish/conf.d/paths.fish
@@ -8,5 +8,8 @@ fish_add_path $HOME/.local/scripts
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
+# Local path
+set --export PATH $HOME/.local/bin $PATH
+
 # add Go bin to PATH (universal, persists)
 set --export PATH $HOME/go/bin $PATH
