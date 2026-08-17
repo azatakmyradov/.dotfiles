e58485e357155b55845d70601e0b2b68f90efb03
Azat Akmyradov <azat@akmyradov.me>
2026-08-17 18:15:10 -0400

    chore(fish): remove OrbStack shell integration

--- stow/core/.config/fish/config.fish
+++ stow/core/.config/fish/config.fish
@@ -28,7 +28,3 @@ if not string match -q -- "$PNPM_HOME/bin" $PATH
   set -gx PATH "$PNPM_HOME/bin" $PATH
 end
 # pnpm end
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
